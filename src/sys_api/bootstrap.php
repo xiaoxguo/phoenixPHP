12f4059d5bc3f6541ae0685f6fabe236ef3c85a2
guojiacheng <guojiacheng@ayibang.com>
2017-04-20 10:57:54 +0800

    fix

--- src/sys_api/bootstrap.php
+++ src/sys_api/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
-include 'frame.php';
+$dir = dirname(__DIR__);
+var_dump($dir);die;
+
 //require __DIR__ . '/../frame/frame.php';
 //PhoenixPHP::run();
---
+++ test.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php
+<?php
+$a = 5 & 6 ;
+
+var_dump($a);
